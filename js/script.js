93145f2d203a2cb0ce9077a4ea60f8611290ab23
Antonio Jackson, SR <ajackson7@hawk.iit.edu>
2020-04-27 13:37:41 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,14 +2,13 @@ function init(){
 	var button = document.getElementById("entrybutton");
 	var input = document.getElementById("entryinput");
 	var output = document.getElementById("textoutput");
-	//var name = document.getElementById("center");
+	var name = document.getElementsByClassName("center")[1];
   
-	button.addEventListener("click", function(){
+	button.addEventListener('click', function(){
 		output.innerHTML = input.value;
-		alert("Antonio Jackson: " + input.value);
+		alert(name.textContent + ": " + input.value);
   });
 
 }
-
 
 window.addEventListener('load', init);
