0756b1d72fb5d722a32b310e511e624741de4d55
Antonio Jackson, SR <ajackson7@hawk.iit.edu>
2020-04-27 12:50:51 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 function init(){
-//add your javascrip between these two lines of code
- 
+  var button = document.getElementById('entrybutton');
+  var entry = document.getElementById('entryinput');
+  var text = document.getElementById('entryoutput');
+  var name = document.getElementById('center');
+  
+  button.addEventListener('click', function(){
+  alert(name.textContent + ": " + entry.value);
+  text.innerHTML = entry.value;)
 
-
-
-
-
+}
 
 
 window.addEventListener('load', init);
